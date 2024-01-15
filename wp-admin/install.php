29aaef45ff3f767c19c12caa5ae0d6cfc57764c3
thekiwismarthome <ksh@mccabe.kiwi>
2024-01-15 13:03:51 +1300

    Publish 2 x Guides

--- wp-admin/install.php
+++ wp-admin/install.php
@@ -5,13 +5,13 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="robots" content="noindex,nofollow">
 	<title>WordPress &rsaquo; Installation</title>
-	<link rel="stylesheet" id="dashicons-css" href="https://thekiwismarthome.github.io/wp-includes/css/dashicons.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://thekiwismarthome.github.io/wp-includes/css/buttons.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="forms-css" href="https://thekiwismarthome.github.io/wp-admin/css/forms.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://thekiwismarthome.github.io/wp-admin/css/l10n.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="install-css" href="https://thekiwismarthome.github.io/wp-admin/css/install.min.css?ver=6.2.2" media="all">
+	<link rel="stylesheet" id="dashicons-css" href="https://thekiwismarthome.github.io/wp-includes/css/dashicons.min.css?ver=6.4.2" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://thekiwismarthome.github.io/wp-includes/css/buttons.min.css?ver=6.4.2" media="all">
+<link rel="stylesheet" id="forms-css" href="https://thekiwismarthome.github.io/wp-admin/css/forms.min.css?ver=6.4.2" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://thekiwismarthome.github.io/wp-admin/css/l10n.min.css?ver=6.4.2" media="all">
+<link rel="stylesheet" id="install-css" href="https://thekiwismarthome.github.io/wp-admin/css/install.min.css?ver=6.4.2" media="all">
 </head>
 <body class="wp-core-ui">
 <p id="logo">WordPress</p>
 
-	<h1>Already Installed</h1><p>You appear to have already installed WordPress. To reinstall please clear your old database tables first.</p><p class="step"><a href="https://thekiwismarthome.github.io/wp-login.php" class="button button-large">Log In</a></p></body></html>
+	<h1>Already Installed</h1><p>You appear to have already installed WordPress. To reinstall please clear your old database tables first.</p><p class="step"><a href="https://thekiwismarthome.github.io/wp-login.php">Log In</a></p></body></html>
