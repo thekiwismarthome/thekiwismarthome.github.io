b1391c48025c9a77e0420d01da3c8a8707b938ec
thekiwismarthome <ksh@mccabe.kiwi>
2024-01-22 14:39:15 +1300

    RPi Voice Assistant Blog

--- wp-admin/install.php
+++ wp-admin/install.php
@@ -5,11 +5,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="robots" content="noindex,nofollow">
 	<title>WordPress &rsaquo; Installation</title>
-	<link rel="stylesheet" id="dashicons-css" href="https://thekiwismarthome.github.io/wp-includes/css/dashicons.min.css?ver=6.4.2" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://thekiwismarthome.github.io/wp-includes/css/buttons.min.css?ver=6.4.2" media="all">
-<link rel="stylesheet" id="forms-css" href="https://thekiwismarthome.github.io/wp-admin/css/forms.min.css?ver=6.4.2" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://thekiwismarthome.github.io/wp-admin/css/l10n.min.css?ver=6.4.2" media="all">
-<link rel="stylesheet" id="install-css" href="https://thekiwismarthome.github.io/wp-admin/css/install.min.css?ver=6.4.2" media="all">
+	<link rel="stylesheet" id="dashicons-css" href="https://thekiwismarthome.github.io/wp-includes/css/dashicons.min.css?ver=6.4.2" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://thekiwismarthome.github.io/wp-includes/css/buttons.min.css?ver=6.4.2" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://thekiwismarthome.github.io/wp-admin/css/forms.min.css?ver=6.4.2" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://thekiwismarthome.github.io/wp-admin/css/l10n.min.css?ver=6.4.2" type="text/css" media="all">
+<link rel="stylesheet" id="install-css" href="https://thekiwismarthome.github.io/wp-admin/css/install.min.css?ver=6.4.2" type="text/css" media="all">
 </head>
 <body class="wp-core-ui">
 <p id="logo">WordPress</p>
